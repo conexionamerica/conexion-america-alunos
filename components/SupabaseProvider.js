8a1f46adade0a70616b282517faae355136f3dec
conexionamerica <tgobarbosa@hotmail.com>
2025-11-07 21:09:45 -0300

    Update SupabaseProvider.js

--- components/SupabaseProvider.js
+++ components/SupabaseProvider.js
@@ -1,11 +1,16 @@
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { useState } from 'react';
 
-// Simplificamos la creación del cliente
-const supabaseClient = createBrowserSupabaseClient();
-
+// Este es el cliente de Supabase que usa las llaves de Vercel
 export default function SupabaseProvider({ children }) {
+  const [supabaseClient] = useState(() =>
+    createPagesBrowserClient({
+      supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+      supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    })
+  );
+
   return (
     <SessionContextProvider supabaseClient={supabaseClient}>
       {children}
